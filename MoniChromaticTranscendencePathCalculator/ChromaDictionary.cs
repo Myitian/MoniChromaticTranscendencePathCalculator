db5fd600e094929bb88f60063f8551feebe4e592
Myitian <miaoyitian233@gmail.com>
2026-04-27 09:27:59 +0800

    file encoding

--- MoniChromaticTranscendencePathCalculator/ChromaDictionary.cs
+++ MoniChromaticTranscendencePathCalculator/ChromaDictionary.cs
@@ -1,4 +1,4 @@
-﻿namespace MoniChromaticTranscendencePathCalculator;
+namespace MoniChromaticTranscendencePathCalculator;
 
 struct ChromaDictionary<T>
 {
--- MoniChromaticTranscendencePathCalculator/PrismaticCore.cs
+++ MoniChromaticTranscendencePathCalculator/PrismaticCore.cs
@@ -1,4 +1,4 @@
-﻿namespace MoniChromaticTranscendencePathCalculator;
+namespace MoniChromaticTranscendencePathCalculator;
 
 enum PrismaticCore
 {
